301a92517b30c1fa7d00a13dfbd012309d9d0143
belevgen <belsky_evgeniy@mail.rum>
2022-09-18 01:05:12 +0600

    HomeWork

---
+++ Task 5.py
@@ -0,0 +1,13 @@
+# Задание 5 Реализуйте алгоритм перемешивания списка.
+
+import random
+
+mylist = [2, 5, 8, 9, 3, 7]
+print(random.sample(mylist, k=6))
+
+# original_list = [input('Введите список: ')]
+# new_list = []
+# for i in original_list:
+#    i = random.randrange(len(original_list))
+#    new_list += i
+# print(new_list)
